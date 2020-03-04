4f282b52efb9f054be88f3aaeebb4f65e4bf6c0b
jdsflk <pergerpatrik113@gmail.com>
2020-03-04 13:09:01 +0100

    átnevezett

--- lib/Screens/InClassroom.dart
+++ lib/Screens/InClassroom.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:loginmodule/Screens/KahootQuestion.dart';
-import 'package:loginmodule/Screens/rdhvtjzbku.dart';
-
 
 import 'Valaszolo.dart';
 
